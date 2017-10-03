20ead4e7e24cbdb7a26a2d399cf5d0cbdebf174a
steven9587 <steven9587@gmail.com>
2017-10-03 16:27:40 +0800

    initial commit

---
+++ src/com/steven/Hello.java
@@ -0,0 +1,13 @@
+package com.steven;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+    
+		person p = new person();
+	    p.sayHello();
+	
+	}
+
+}
---
+++ src/com/steven/person.java
@@ -0,0 +1,7 @@
+package com.steven;
+
+public class person {
+       public void sayHello(){
+       System.out.println("Hello!");
+       }
+}
