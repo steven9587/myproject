5d80f7115a9f9b788498f4eefdda5a020beb6ea7
steven9587 <steven9587@gmail>
2017-11-28 16:28:11 +0800

    排序

---
+++ src/com/steven/Sorting.java
@@ -0,0 +1,20 @@
+package com.steven;
+
+public class Sorting {
+
+	public static void main(String[] args) {
+		int n[] = { 51, 23, 6, 65, 15 };
+		for (int i = 0; i < n.length; i++) {
+			
+			for (int f = 1; f < n.length; f++) {
+				if (n[f] < n[i]) {
+					int tmp = n[f];
+					n[f] = n[i];
+					n[i] = tmp;
+				}
+			}
+			
+			System.out.println(n[i]);
+		}
+	}
+}
