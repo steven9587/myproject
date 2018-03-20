7dbaaf3c2bba4aed2e85e048c6be0bafaec8e924
steven9587 <steven9587@gmail.com>
2018-03-20 15:31:13 +0800

    ABGameScanner

--- src/com/java2/object/ABGameTeacher.java
+++ src/com/java2/object/ABGameTeacher.java
@@ -1,29 +1,38 @@
 package com.java2.object;
 
+import java.util.Scanner;
+
 public class ABGameTeacher {
 
 	public static void main(String[] args) {
 		int b = 0;
 		int a = 0;
 
-		String secret = "9873";
-		String nums = "7893";
-		int length = secret.length();
+		while (a != 4) {
+			a = 0 ;
+			b = 0 ;
+			System.out.print("please enter your guess：");
+			Scanner scanner = new Scanner(System.in);
+			String nums = scanner.nextLine();
 
-		for (int i = 0; i < length; i++) {
-			for (int f = 0; f < length; f++) {
-				char s = secret.charAt(i);
-				char n = nums.charAt(f);
-				if (s == n) {
-					if (i == f) {
-						a = a + 1;
-					} else {
-						b = b + 1;
+			String secret = "9873";
+			int length = secret.length();
+
+			for (int i = 0; i < length; i++) {
+				for (int f = 0; f < length; f++) {
+					char s = secret.charAt(i);
+					char n = nums.charAt(f);
+					if (s == n) {
+						if (i == f) {
+							a = a + 1;
+						} else {
+							b = b + 1;
+						}
 					}
 				}
 			}
+			System.out.println(a + "A" + b + "B");
 		}
-		System.out.println(a + "A" + b + "B");
-	}
 
+	}
 }
