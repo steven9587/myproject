dadd98acb92f6006ccd35d08e7023720639f6816
steven9587 <steven9587@gmail.com>
2018-04-10 16:25:29 +0800

    filetester

---
+++ src/com/java2/io/FileTester.java
@@ -0,0 +1,23 @@
+package com.java2.io;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileTester {
+
+	public static void main(String[] args) {
+		try {
+			FileOutputStream out = new FileOutputStream("data.txt");
+			out.write(65);
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("file not");
+			;
+		} catch (IOException e) {
+			System.out.println("file not");
+		}
+	}
+
+}
