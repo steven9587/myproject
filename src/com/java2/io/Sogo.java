803016b4387da8a2616859bccd2ee9ce9fdca33c
steven9587 <steven9587@gmail.com>
2018-04-17 15:42:35 +0800

    Sogosales-2

--- src/com/java2/io/Sogo.java
+++ src/com/java2/io/Sogo.java
@@ -1,9 +1,20 @@
 package com.java2.io;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import customer.Customer;
+import customer.GlodenCustomer;
+import customer.SilverCustomer;
+
 
 public class Sogo {
 
@@ -22,6 +33,49 @@ public class Sogo {
 			case 1:
 				getsales();
 				break;
+			case 2:
+				List<Sales> list = new ArrayList<>();
+				try {
+					FileInputStream fis = new FileInputStream("sales.txt");
+					InputStreamReader isr = new InputStreamReader(fis);
+					BufferedReader in = new BufferedReader(isr);
+					String line = in.readLine();
+					while (line != null) {
+						String[] token = line.split("\t");
+						try {
+							int type = Integer.parseInt(token[0]);
+							int amount = Integer.parseInt(token[1]);
+							Sales sales = new Sales(type, amount);
+							list.add(sales);
+						} catch (NumberFormatException e) {
+							System.out.println("資料格式錯誤");
+							return;
+						}
+						line = in.readLine();
+					}
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				// report
+				for (Sales sales: list){
+					Customer customer = null;
+					switch(sales.type){
+					case 1: 
+						customer = new Customer(sales.getAmount());
+						break;
+					case 2:
+						customer = new SilverCustomer(sales.getAmount());
+						break;
+					case 3:
+						customer = new GlodenCustomer(sales.getAmount());
+						break;
+					}
+					customer.print();
+				}
 			case 3:
 				return;
 			}
