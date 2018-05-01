d788eaa9013e9d479c38773ebead0236bcccf1c6
steven9587 <steven9587@gmail.com>
2018-05-01 16:21:08 +0800

    20180501Homework

---
+++ src/com/java2/object/Stepik.java
@@ -0,0 +1,33 @@
+package com.java2.object;
+
+public class Stepik {
+
+		class Employee {
+
+			  // write fields
+
+			  // write constructor
+
+			  // write getters
+			}
+
+			class Developer extends Employee {
+
+			  // write fields
+
+			  // write constructor
+
+			  // write getters
+			}
+
+			class DataAnalyst extends Employee {
+
+			  // write fields
+
+			  // write constructor
+
+			  // write getters
+			}
+
+	}
+
