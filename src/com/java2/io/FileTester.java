808a266e0ddd6f6c4d1527e3b86bd0c1f1fc63f6
steven9587 <steven9587@gmail.com>
2018-05-01 14:23:54 +0800

    20180501往上層拋出例外

--- src/com/java2/io/FileTester.java
+++ src/com/java2/io/FileTester.java
@@ -6,20 +6,26 @@ import java.io.IOException;
 
 public class FileTester {
 
-	public static void main(String[] args) {
-		try {
-			FileOutputStream out = new FileOutputStream("data.txt");
-			out.write(65);
-			out.write(66);
-			out.write(67);
-			out.flush();
-			out.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("File Not Found! ");
-			;
-		} catch (IOException e) {
-			System.out.println("file Output Problem ");
-		}
+	//丟上來可用try...catch處理或繼續throws
+	public static void main(String[] args) throws IOException {
+		write();
 	}
 
+	// 若簡單則在下方直接用try...catch解決
+	// 若困難丟到main方法理處理
+	public static void write() throws IOException {
+		/*
+		 * try { FileOutputStream out = new FileOutputStream("data.txt");
+		 * out.write(65); out.write(66); out.write(67); out.flush();
+		 * out.close(); } catch (FileNotFoundException e) {
+		 * System.out.println("File Not Found! "); ; } catch (IOException e) {
+		 * System.out.println("file Output Problem "); } }
+		 */
+		FileOutputStream out = new FileOutputStream("data.txt");
+		out.write(65);
+		out.write(66);
+		out.write(67);
+		out.flush();
+		out.close();
+	}
 }
