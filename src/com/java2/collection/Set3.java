90c39fa12288a94a89dd01c7d622d6454dee1d92
steven9587 <steven9587@gmail.com>
2018-05-15 16:30:49 +0800

    thread

--- src/com/java2/collection/Set3.java
+++ src/com/java2/collection/Set3.java
@@ -1,19 +1,15 @@
 package com.java2.collection;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Set3 {
 
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<>(
-				Arrays.asList("postgres", "sqlite", "oracle", "mongodb", "postgres", "mssql"));
-		System.out.println(list.size());
 		Set<String> nameSet = new TreeSet<>(
-				Arrays.asList("postgres", "sqlite", "oracle", "mongodb", "postgres", "mssql"));
+				Arrays.asList("6","postgres", "sqlite", "oracle", "mongodb", "postgres", "mssql"));
+		nameSet.remove("6");
 		for (String name : nameSet) {
 			System.out.println(name);
 		}
