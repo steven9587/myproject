415ae3c036321a746247ea9e8b79cf221420768d
steven9587 <steven9587@gmail.com>
2018-06-05 22:09:53 +0800

    maze-1

---
+++ src/com/java2/oo/MazeMain.java
@@ -0,0 +1,20 @@
+package com.java2.oo;
+
+public class MazeMain {
+/*
+ * 有一遊戲場地，長寬由執行時決定，場地中亂數產生陷阱
+ * 陷阱數也由當時才決定
+ * 
+ * 有一個玩家在這場地中，初始PH值為100，走一步消耗1HP，
+ * 撞到邊界扣5HP，掉到陷阱扣20HP
+ * 
+ * 有個檔案(maze.txt)資訊如下：
+ * 6,4
+ * 5
+ * 6,8,2,4,2,2,6,6,8,4,2,2,6
+ */
+	public static void main(String[] args) {
+
+	}
+
+}
