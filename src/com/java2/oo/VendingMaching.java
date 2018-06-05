1f52c672bc6fa5422c90c2bee7880d86aab8959e
steven9587 <steven9587@gmail.com>
2018-06-05 21:54:02 +0800

    VendingMachine-final

--- src/com/java2/oo/VendingMaching.java
+++ src/com/java2/oo/VendingMaching.java
@@ -16,25 +16,54 @@ public class VendingMaching {
 		}
 		Scanner scanner = new Scanner(System.in);
 		int z = -1;
-		int m = 0;
+		int total = 0;
 		while (z != 0) {
-			System.out.println("販賣機餘額:" + " " + m + "元");
-			System.out.println("請投幣或選擇飲料(a或b或c)，或輸入0結束:");
+			System.out.println("餘額:" + total + "元");
+			System.out.println("請投幣($5或$10或$50)或選擇飲料(1或2或3)，或輸入0結束:");
 			String line = scanner.nextLine();
-
 			switch (line) {
 			case "1":
-				m = m + 1;
+				if (total >= 25) {
+					Drink drink = drinks.get(0);
+					System.out.println("獲得：" + drink.getName());
+					total -=drink.getprice();
+				} else {
+					System.out.println("餘額不足!");
+				}
+				break;
+			case "2":
+				if (total >= 30) {
+					Drink drink = drinks.get(1);
+					System.out.println("獲得：" + drink.getName());
+					total -=drink.getprice();
+				} else {
+					System.out.println("餘額不足!");
+				}
+				break;
+			case "3":
+				if (total >= 20) {
+					Drink drink = drinks.get(2);
+					System.out.println("獲得：" + drink.getName());
+					total -=drink.getprice();
+				} else {
+					System.out.println("餘額不足!");
+				}
 				break;
 			case "5":
-				m = m + 5;
+				total = total + 5;
 				break;
 			case "10":
-				m = m + 10;
+				total = total + 10;
+				break;
+			case "50":
+				total = total + 50;
 				break;
 			case "0":
-				System.out.println("結束!");
+				System.out.println("關機!");
 				z = 0;
+				break;
+			default:
+				System.out.println("無效輸入!");
 				break;
 			}
 
--- src/com/java2/oo/VendingMain.java
+++ src/com/java2/oo/VendingMain.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-
 public class VendingMain {
 
 	public static void main(String[] args) {
@@ -15,22 +14,21 @@ public class VendingMain {
 			FileReader fr = new FileReader("vending.txt");
 			BufferedReader br = new BufferedReader(fr);
 			String line = br.readLine();
-				
-				String[] tokens = line.split(",");
-				int count = Integer.parseInt(tokens[0]);
-				for (int i = 1; i <= 3; i++) {
-					String name = tokens[i * 2 - 1];
-					int price = Integer.parseInt(tokens[i * 2]);
-					vm.drinks.add(new Drink(i, name, price));
-				}
-				vm.on();
+			String[] tokens = line.split(",");
+			int count = Integer.parseInt(tokens[0]);
+			for (int i = 1; i <= count; i++) {
+				String name = tokens[i * 2 - 1];
+				int price = Integer.parseInt(tokens[i * 2]);
+				vm.drinks.add(new Drink(i, name, price));
+			}
+			vm.on();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}				
+		}
 	}
 
 }
