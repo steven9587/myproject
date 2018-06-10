cbf75950c4f70bb10268dcef292e8b0e47829a63
steven9587 <steven9587@gmail.com>
2018-06-10 16:45:30 +0800

    bingo-3

--- src/com/java2/bingo/BingoMain.java
+++ src/com/java2/bingo/BingoMain.java
@@ -17,15 +17,20 @@ public class BingoMain {
 			String line = br.readLine();
 			int number = Integer.parseInt(line);
 			int table = (int) Math.sqrt(number);
-			// System.out.println(number);
-			// System.out.println(table);
-			Form form = new Form(table, number);
+			line = br.readLine();
+			// System.out.println(line);
+			String tokens[] = line.split(",");
+			List<Integer> answers = new ArrayList<>();
+			for (int i = 0; i <= tokens.length - 1; i++) {
+				int answer = Integer.parseInt(tokens[i]);
+				answers.add(answer);
+			}
+			Form form = new Form(table, number, answers);
 			form.print();
+			// System.out.println(answers);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -37,30 +42,44 @@ public class BingoMain {
 	public class Form {
 		int table;
 		int number;
+		List<Integer> answers;
 
-		public Form(int table, int number) {
+		public Form(int table, int number, List<Integer> answers) {
 			this.table = table;
 			this.number = number;
-
+			this.answers = answers;
 		}
 
 		public void print() {
 			List<Integer> numbers = new ArrayList<>();
 			for (int i = 1; i <= number; i++) {
 				numbers.add(i);
-				Collections.shuffle(numbers);
 			}
-			for (int p = 0; p < numbers.size(); p++) {
-				if (numbers.get(p) > 9) {
-					System.out.print(numbers.get(p) + " ");
-				} else {
-					System.out.print(" " + numbers.get(p) + " ");
+			Collections.shuffle(numbers);
+			int[][] finaltable = new int[table][table];
+			for (int i = 0; i < table; i++) {
+				for (int j = 0; j < table; j++) {
+					finaltable[i][j] = numbers.get(i * table + j);
+					if (finaltable[i][j] > 9) {
+						System.out.print(finaltable[i][j] + " ");
+					} else {
+						System.out.print(" " + finaltable[i][j] + " ");
+					}
 				}
-				if (p % 5 == 4) {
-					System.out.println();
-				}
+				System.out.println();
 			}
+
+			/*
+			 * 用list方法印出 for (int p = 0; p < numbers.size(); p++) { if (numbers.get(p) > 9)
+			 * {
+			 * 
+			 * System.out.print(numbers.get(p) + " "); } else { System.out.print(" " +
+			 * numbers.get(p) + " "); } if (p % 5 == 4) { System.out.println(); } }
+			 */
 		}
 
+		public void bingo() {
+
+		}
 	}
 }
