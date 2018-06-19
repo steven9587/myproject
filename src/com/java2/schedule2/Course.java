8e68c8f0c34d54bd22b2691838b9f217c1373bc9
steven9587 <steven9587@gmail.com>
2018-06-19 16:30:26 +0800

    newschedule

---
+++ src/com/java2/schedule2/Course.java
@@ -0,0 +1,5 @@
+package com.java2.schedule2;
+
+public class Course {
+
+}
---
+++ src/com/java2/schedule2/Main.java
@@ -0,0 +1,15 @@
+package com.java2.schedule2;
+
+public class Main {
+	public Main() {
+		readCourse();
+	}
+
+	public void readCourse() {
+	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
+
+}
